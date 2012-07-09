d4c2833a9c1e9d4568af6bb5127dd56c7accfd12
Bailey Mihajlich <baileymihajlich@gmail.com>
2012-07-08 20:32:55 -0700

    Prototype 0.1: Movement, Collision detection w/ obstacles, and music

---
+++ README.txt
@@ -0,0 +1,11 @@
+Mechanical Failure - Readme
+
+System Requirements:
+	PC, Mac, or Linux
+
+Requirements:
+	Python 2, 32-bit version: http://www.python.org/download/
+	PyGame: http://www.pygame.org/download.shtml
+
+To Run the game:
+	Double-click on main.py
--- main.py
+++ main.py
@@ -55,14 +55,14 @@ newpos = (CENTERCENTER[0] - 100, CENTERCENTER[1] + 100)
 barrel3 = Obstacle("barrel", "terrain", images, newpos, True)
 mapGroup.add(barrel, barrel2, barrel3)
 # set up npc
-newpos = (CENTERCENTER[0] + 100, CENTERCENTER[1] + 100)
-rat = NPC("Ratty", "rat", images, newpos, solidGroup)
-npcGroup.add(rat)
+#newpos = (CENTERCENTER[0] + 100, CENTERCENTER[1] + 100)
+#rat = NPC("Ratty", "rat", images, newpos, solidGroup)
+#npcGroup.add(rat)
 # set up groups
-characterGroup.add(npcGroup)
+#characterGroup.add(npcGroup)
 solidGroup.add(mapGroup)
 solidGroup.add(characterGroup)
-solidGroup.add(npcGroup)
+#solidGroup.add(npcGroup)
 
 while True:
     # handle game events
@@ -79,7 +79,7 @@ while True:
 
     # update the sprites
     hero.update(solidGroup)
-    npcGroup.update(solidGroup)
+    #npcGroup.update(solidGroup)
 
     # update the screen
     drawground(window, tile)
