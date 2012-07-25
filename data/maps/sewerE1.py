76a723b2bbf1941bd36ffb1676d6cdcaa79c2165
Bailey Mihajlich <baileymihajlich@gmail.com>
2012-07-24 22:55:08 -0700

    added level end for prototype presentation purpose

---
+++ data/maps/sewerE1.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2012 Bailey Mihajlich
+# Licensed under the GNU GPL v.2
+# See license.txt for licence information
+
+#self.type = "slum"
+#self.type = "house"
+self.type = "sewer"
+
+#self.script = ["doors"]
+self.script = ["Thats the end so far, thanks for playing!"]
+
+#self.npcname = ""
+#self.npcref = ""
+#self.npclines = [""]
+
+#self.sign = ""
+
+self.east = ""
+self.west = "sewerCenter"
+self.north = ""
+self.south = ""
+self.up = ""
+self.down = ""
+
+self.grid = ["WWWWWWWWWWWWWWWW",
+             "W..............W",
+             "W..............W",
+             "W..............W",
+             "W..............W",
+             "W..............W",
+             "W..............W",
+             "...............W",
+             "...............W",
+             "W..............W",
+             "W..............W",
+             "W..............W",
+             "W..............W",
+             "W..............W",
+             "W..............W",
+             "WWWWWWWWWWWWWWWW",]
